4eb23a2a5aee01e78375a33246682dd1072bdf94
Emery Parks <emeryparks@protonmail.com>
2024-11-12 13:31:35 -0500

    more basic vulkan stuff

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -50,10 +50,26 @@ struct renderer {
         struct queue_family {
             uint32_t index; /* the index of each family */
             bool exists; /* whether the family exists */
-        } graphics; /* the graphics queue family */
+        } graphics, /* the graphics queue family */
+          present; /* the presentation queue family */
     } queue_families; /* the queue families */
 
     VkQueue graphics_queue; /* the graphics queue */
+
+    VkSurfaceKHR surface; /* the window surface */
+
+    struct swap_chain_details {
+        VkSurfaceCapabilitiesKHR capabilities;
+        VkSurfaceFormatKHR * formats;
+        VkSurfaceFormatKHR format;
+        uint32_t  n_formats;
+        VkPresentModeKHR * present_modes;
+        VkPresentModeKHR present_mode;
+        uint32_t n_present_modes;
+        VkExtent2D extent;
+    } chain_details; /* information about the swap chain */
+
+    VkSwapchainKHR swap_chain;
 
 } renderer = { };
 
@@ -83,7 +99,17 @@ static void print_missing_layers(
     (void)length;
     (void)data;
     (void)ptr;
-    fprintf(stderr, "[renderer] missing layer %s\n", key);
+    fprintf(stderr, "[renderer] (INFO) missing layer %s\n", key);
+}
+
+/* callback for setup_physical_device() */
+static void print_missing_extensions(
+        const char * key, size_t length, void * data, void * ptr)
+{
+    (void)length;
+    (void)data;
+    (void)ptr;
+    fprintf(stderr, "[renderer] (INFO) missing extension %s\n", key);
 }
 
 /* initialize the vulkan instance and extensions */
@@ -178,6 +204,255 @@ static enum renderer_init_result setup_instance()
     return RENDERER_INIT_OKAY;
 }
 
+/* have GLFW create a window sruface */
+static enum renderer_init_result setup_window_surface()
+{
+    VkResult result = glfwCreateWindowSurface(
+            renderer.instance,
+            renderer.window,
+            NULL,
+            &renderer.surface
+        );
+
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "[renderer] glfwCreateWindowSurface() failed\n");
+        renderer_terminate();
+        return RENDERER_INIT_ERROR;
+    }
+
+    return RENDERER_INIT_OKAY;
+}
+
+/* find appropriate queue families (using a candidate physical device)
+ *
+ * this doesn't terminate on error because we might be able to try again with
+ * a different device
+ */
+static enum renderer_init_result setup_queue_families(
+        VkPhysicalDevice candidate)
+{
+    uint32_t n_queue_families;
+    vkGetPhysicalDeviceQueueFamilyProperties(
+            candidate, &n_queue_families, NULL);
+    VkQueueFamilyProperties * queue_families =
+        malloc(sizeof(*queue_families) * n_queue_families);
+    vkGetPhysicalDeviceQueueFamilyProperties(
+            candidate, &n_queue_families, queue_families);
+
+    for (size_t i = 0; i < n_queue_families; i++) {
+        if (!renderer.queue_families.graphics.exists) {
+            if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                renderer.queue_families.graphics.index = i;
+                renderer.queue_families.graphics.exists = true;
+            }
+        }
+
+        if (!renderer.queue_families.present.exists) {
+            VkBool32 can_present;
+            vkGetPhysicalDeviceSurfaceSupportKHR(
+                    candidate, i, renderer.surface, &can_present);
+            if (can_present) {
+                renderer.queue_families.present.index = i;
+                renderer.queue_families.present.exists = true;
+            }
+        }
+    }
+
+    free(queue_families);
+
+    if (!renderer.queue_families.graphics.exists) {
+        fprintf(
+                stderr,
+                "[renderer] (INFO) candidate device lacks graphics bit\n"
+            );
+        return RENDERER_INIT_ERROR;
+    }
+
+    if (!renderer.queue_families.present.exists) {
+        fprintf(
+                stderr,
+                "[renderer] (INFO) candidate device cannot present to surface\n"
+            );
+        return RENDERER_INIT_ERROR;
+    }
+
+    return RENDERER_INIT_OKAY;
+}
+
+/* set up the swap chain */
+static enum renderer_init_result setup_swap_chain()
+{
+    /* prefer SRGB R8G8B8 */
+    renderer.chain_details.format = renderer.chain_details.formats[0];
+    for (uint32_t i = 0; i < renderer.chain_details.n_formats; i++) {
+        if (renderer.chain_details.formats[i].format ==
+                VK_FORMAT_B8G8R8A8_SRGB &&
+                renderer.chain_details.formats[i].colorSpace ==
+                VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+            renderer.chain_details.format = renderer.chain_details.formats[i];
+            break;
+        }
+    }
+
+    /* prefer MAILBOX */
+    renderer.chain_details.present_mode = VK_PRESENT_MODE_FIFO_KHR;
+    for (uint32_t i = 0; i < renderer.chain_details.n_present_modes; i++) {
+        if (renderer.chain_details.present_modes[i] ==
+                VK_PRESENT_MODE_MAILBOX_KHR) {
+            renderer.chain_details.present_mode =
+                renderer.chain_details.present_modes[i];
+        }
+    }
+
+    if (renderer.chain_details.capabilities.currentExtent.width !=
+            UINT32_MAX) {
+        renderer.chain_details.extent =
+            renderer.chain_details.capabilities.currentExtent;
+    } else {
+        int width, height;
+        glfwGetFramebufferSize(renderer.window, &width, &height);
+
+        VkExtent2D extent = {
+            .width = width,
+            .height = height
+        };
+
+        if (extent.width <
+                renderer.chain_details.capabilities.minImageExtent.width) {
+            extent.width =
+                renderer.chain_details.capabilities.minImageExtent.width;
+        } else if (extent.width >
+                renderer.chain_details.capabilities.maxImageExtent.width) {
+            extent.width =
+                renderer.chain_details.capabilities.maxImageExtent.width;
+        }
+
+        if (extent.height <
+                renderer.chain_details.capabilities.minImageExtent.height) {
+            extent.height =
+                renderer.chain_details.capabilities.minImageExtent.height;
+        } else if (extent.height >
+                renderer.chain_details.capabilities.maxImageExtent.height) {
+            extent.height =
+                renderer.chain_details.capabilities.maxImageExtent.height;
+        }
+
+        renderer.chain_details.extent = extent;
+    }
+
+    VkSwapchainCreateInfoKHR create_info = {
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = renderer.surface,
+        .minImageCount =
+            renderer.chain_details.capabilities.minImageCount + 1,
+        .imageFormat = renderer.chain_details.format.format,
+        .imageColorSpace = renderer.chain_details.format.colorSpace,
+        .imageExtent = renderer.chain_details.extent,
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .preTransform =
+            renderer.chain_details.capabilities.currentTransform,
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = renderer.chain_details.present_mode,
+        .clipped = VK_TRUE,
+        .oldSwapchain = VK_NULL_HANDLE
+    };
+
+    if (renderer.queue_families.graphics.index !=
+            renderer.queue_families.present.index) {
+        create_info.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+        create_info.queueFamilyIndexCount = 2;
+        create_info.pQueueFamilyIndices = (uint32_t[]){
+            renderer.queue_families.graphics.index,
+            renderer.queue_families.present.index
+        };
+    } else {
+        create_info.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+    }
+
+    VkResult result = vkCreateSwapchainKHR(
+                renderer.device, &create_info, NULL, &renderer.swap_chain);
+
+    if (result != VK_SUCCESS) {
+        fprintf(stderr, "[renderer] vkCreateSwapchainKHR failed\n");
+        renderer_terminate();
+        return RENDERER_INIT_ERROR;
+    }
+
+    return RENDERER_INIT_OKAY;
+}
+
+/* test if this candidate supports the window surface/swap chain
+ *
+ * this doesn't terminate on error because it might be called with another
+ * candidate
+ */
+static enum renderer_init_result setup_swap_chain_details(
+        VkPhysicalDevice candidate)
+{
+    vkGetPhysicalDeviceSurfaceCapabilitiesKHR(
+            candidate,
+            renderer.surface,
+            &renderer.chain_details.capabilities
+        );
+
+    vkGetPhysicalDeviceSurfaceFormatsKHR(
+            candidate,
+            renderer.surface,
+            &renderer.chain_details.n_formats,
+            NULL
+        );
+
+    if (renderer.chain_details.formats) {
+        free(renderer.chain_details.formats);
+    }
+
+    renderer.chain_details.formats =
+        malloc(sizeof(*renderer.chain_details.formats) *
+                renderer.chain_details.n_formats);
+
+    vkGetPhysicalDeviceSurfaceFormatsKHR(
+            candidate,
+            renderer.surface,
+            &renderer.chain_details.n_formats,
+            renderer.chain_details.formats
+        );
+
+    vkGetPhysicalDeviceSurfacePresentModesKHR(
+            candidate,
+            renderer.surface,
+            &renderer.chain_details.n_present_modes,
+            NULL
+        );
+
+    if (renderer.chain_details.present_modes) {
+        free(renderer.chain_details.present_modes);
+    }
+
+    renderer.chain_details.present_modes =
+        malloc(sizeof(*renderer.chain_details.present_modes) *
+                renderer.chain_details.n_present_modes);
+
+    vkGetPhysicalDeviceSurfacePresentModesKHR(
+            candidate,
+            renderer.surface,
+            &renderer.chain_details.n_present_modes,
+            renderer.chain_details.present_modes
+        );
+
+    if (renderer.chain_details.n_formats == 0) {
+        fprintf(stderr, "[renderer] (INFO) device has no formats for this surface\n");
+        return RENDERER_INIT_ERROR;
+    }
+
+    if (renderer.chain_details.n_present_modes == 0) {
+        fprintf(stderr, "[renderer] (INFO) device has no present modes for this surface\n");
+        return RENDERER_INIT_ERROR;
+    }
+
+    return RENDERER_INIT_OKAY;
+}
+
 /* pick a physical device */
 static enum renderer_init_result setup_physical_device()
 {
@@ -192,35 +467,89 @@ static enum renderer_init_result setup_physical_device()
     VkPhysicalDevice * devices = malloc(sizeof(*devices) * n_devices);
     vkEnumeratePhysicalDevices(renderer.instance, &n_devices, devices);
 
+    struct sorted_set * required_extensions_set = sorted_set_create();
+    sorted_set_add_key_copy(
+            required_extensions_set, VK_KHR_SWAPCHAIN_EXTENSION_NAME, 0, NULL);
+
     VkPhysicalDevice candidate = NULL;
     /* find a suitable device */
     /* TODO: user override */
     for (size_t i = 0; i < n_devices; i++) {
         VkPhysicalDeviceProperties device_properties;
         vkGetPhysicalDeviceProperties(devices[i], &device_properties);
-        /*
-        VkPhysicalDeviceFeatures device_features;
-        vkGetPhysicalDeviceFeatures(devices[i], &device_features);
-        */
         fprintf(
-                stdout,
-                "[renderer] found device %s\n",
+                stderr,
+                "[renderer] (INFO) found physical device %s\n",
                 device_properties.deviceName
             );
 
-        if (device_properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+        uint32_t n_extensions;
+        vkEnumerateDeviceExtensionProperties(
+                devices[i], NULL, &n_extensions, NULL);
+        VkExtensionProperties * extensions =
+            malloc(sizeof(*extensions) * n_extensions);
+        vkEnumerateDeviceExtensionProperties(
+                devices[i], NULL, &n_extensions, extensions);
+
+        struct sorted_set * extensions_set = sorted_set_create();
+
+        for (size_t i = 0; i < n_extensions; i++ ) {
+            sorted_set_add_key_copy(
+                    extensions_set, extensions[i].extensionName, 0, NULL);
+        }
+
+        free(extensions);
+
+        struct sorted_set * missing_set =
+            sorted_set_difference(required_extensions_set, extensions_set);
+
+        if (sorted_set_size(missing_set)) {
+            sorted_set_apply(missing_set, &print_missing_extensions, NULL);
+            sorted_set_destroy(missing_set);
+            sorted_set_destroy(extensions_set);
+            continue;
+        }
+
+        sorted_set_destroy(missing_set);
+        sorted_set_destroy(extensions_set);
+
+        if (setup_queue_families(devices[i])) {
+            continue;
+        }
+
+        if (setup_swap_chain_details(devices[i])) {
+            continue;
+        }
+
+        if (device_properties.deviceType ==
+                VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
             candidate = devices[i];
         } else if (!candidate) {
             candidate = devices[i];
         }
     }
+
+    sorted_set_destroy(required_extensions_set);
     free(devices);
+
+    if (!candidate) {
+        fprintf(
+                stderr,
+                "[renderer] no suitable physical dveices found\n"
+            );
+        renderer_terminate();
+        return RENDERER_INIT_ERROR;
+    }
+
+    /* run it again now that we've picked */
+    setup_queue_families(candidate);
+    setup_swap_chain_details(candidate);
 
     VkPhysicalDeviceProperties device_properties;
     vkGetPhysicalDeviceProperties(candidate, &device_properties);
     fprintf(
-            stdout,
-            "[renderer] picked device %s (discrete: %s)\n",
+            stderr,
+            "[renderer] (INFO) picked device %s (discrete: %s)\n",
             device_properties.deviceName,
             device_properties.deviceType ==
                 VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU ? "true" : "false"
@@ -231,38 +560,8 @@ static enum renderer_init_result setup_physical_device()
     return RENDERER_INIT_OKAY;
 }
 
-enum renderer_init_result setup_queue_families()
-{
-    uint32_t n_queue_families;
-    vkGetPhysicalDeviceQueueFamilyProperties(
-            renderer.physical_device, &n_queue_families, NULL);
-    VkQueueFamilyProperties * queue_families =
-        malloc(sizeof(*queue_families) * n_queue_families);
-    vkGetPhysicalDeviceQueueFamilyProperties(
-            renderer.physical_device, &n_queue_families, queue_families);
-
-    for (size_t i = 0; i < n_queue_families; i++) {
-        if (queue_families[i].queueFlags & VK_QUEUE_GRAPHICS_BIT) {
-            renderer.queue_families.graphics.index = i;
-            renderer.queue_families.graphics.exists = true;
-        }
-    }
-
-    free(queue_families);
-
-    if (!renderer.queue_families.graphics.exists) {
-        fprintf(
-                stderr,
-                "[renderer] no queue family has the graphics bit set\n"
-            );
-        renderer_terminate();
-        return RENDERER_INIT_ERROR;
-    }
-
-    return RENDERER_INIT_OKAY;
-}
-
-enum renderer_init_result setup_logical_device()
+/* create a logical device */
+static enum renderer_init_result setup_logical_device()
 {
     VkDeviceQueueCreateInfo queue_create_info[] = {
         {
@@ -275,12 +574,18 @@ enum renderer_init_result setup_logical_device()
 
     VkPhysicalDeviceFeatures device_features = { };
 
+    const char * extensions[] = {
+        VK_KHR_SWAPCHAIN_EXTENSION_NAME
+    };
+
     VkDeviceCreateInfo device_create_info = {
         .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
         .pQueueCreateInfos = queue_create_info,
         .queueCreateInfoCount =
             sizeof(queue_create_info) / sizeof(*queue_create_info),
-        .pEnabledFeatures = &device_features
+        .pEnabledFeatures = &device_features,
+        .enabledExtensionCount = sizeof(extensions) / sizeof(*extensions),
+        .ppEnabledExtensionNames = extensions
     };
 
     VkResult result = vkCreateDevice(
@@ -291,7 +596,7 @@ enum renderer_init_result setup_logical_device()
         );
 
     if (result != VK_SUCCESS) {
-        fprintf(stderr, "[renderer vkCreateDevice() failed\n");
+        fprintf(stderr, "[renderer] vkCreateDevice() failed\n");
         renderer_terminate();
         return RENDERER_INIT_ERROR;
     }
@@ -306,6 +611,7 @@ enum renderer_init_result setup_logical_device()
     return RENDERER_INIT_OKAY;
 }
 
+
 /* initialize the renderer */
 enum renderer_init_result renderer_init()
 {
@@ -317,13 +623,16 @@ enum renderer_init_result renderer_init()
     result = setup_instance();
     if (result) return result;
 
+    result = setup_window_surface();
+    if (result) return result;
+
     result = setup_physical_device();
     if (result) return result;
 
-    result = setup_queue_families();
+    result = setup_logical_device();
     if (result) return result;
 
-    result = setup_logical_device();
+    result = setup_swap_chain();
     if (result) return result;
 
     renderer.initialized = true;
@@ -335,6 +644,25 @@ void renderer_terminate()
 {
     renderer.initialized = false;
 
+    if (renderer.swap_chain) {
+        vkDestroySwapchainKHR(renderer.device, renderer.swap_chain, NULL);
+        renderer.swap_chain = NULL;
+    }
+
+    if (renderer.chain_details.formats) {
+        free(renderer.chain_details.formats);
+        renderer.chain_details.formats = NULL;
+        renderer.chain_details.n_formats = 0;
+        renderer.chain_details.format = (VkSurfaceFormatKHR) { };
+    }
+
+    if (renderer.chain_details.present_modes) {
+        free(renderer.chain_details.present_modes);
+        renderer.chain_details.present_modes = NULL;
+        renderer.chain_details.n_present_modes = 0;
+        renderer.chain_details.present_mode = (VkPresentModeKHR) { };
+    }
+
     if (renderer.device) {
         vkDestroyDevice(renderer.device, NULL);
         renderer.device = NULL;
@@ -343,12 +671,17 @@ void renderer_terminate()
     }
 
     /* VkPhysicalDevice doesn't have a separate destroy */
+    renderer.physical_device = NULL;
+    renderer.queue_families = (struct queue_families) { };
+
+    if (renderer.surface) {
+        vkDestroySurfaceKHR(renderer.instance, renderer.surface, NULL);
+        renderer.surface = NULL;
+    }
 
     if (renderer.instance) {
         vkDestroyInstance(renderer.instance, NULL);
         renderer.instance = NULL;
-        renderer.physical_device = NULL;
-        renderer.queue_families = (struct queue_families) { };
     }
 
     if (renderer.layers) {
